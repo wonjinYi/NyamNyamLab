8447b1ee0e400da69fe5d60371911043d9bb6ca0
wonjinYi <studioplug17@gmail.com>
2021-02-28 04:42:44 +0900

    clean up. loading feedback works.

--- test/src/components/Maps.js
+++ test/src/components/Maps.js
@@ -2,12 +2,14 @@
 import { React, useState, useEffect } from "react";
 import ScriptTag from "react-script-tag";
 import axios from "axios";
+
 import styled from "styled-components";
 
 // imported components ==========================================
-// *
+import Loading from "./atoms/Loading";
 
 // Main Component ===============================================
+let map = null;
 const mapValues = {
     nyamListSource : "https://script.google.com/macros/s/AKfycbyuACrtmBSgcaehSnfbDVhSAMeScTP455Z44h4jtjxRGxJ5UNeVdOEh/exec",
     mapSource : "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ont9t74d67",
@@ -19,57 +21,15 @@ const mapValues = {
 }
 
 export default function Maps () {
-    let map = null;
-    let markers = [];
-
-    const init = async() => {
-        // set naver.maps.Map
-        const { center, zoom } = mapValues;
-
-        const mapOptions = {
-            center: new window.naver.maps.LatLng(center.lat, center.lng),
-            zoom: zoom,
-        };
-        map = new window.naver.maps.Map("map", mapOptions);   
-
-        // read nyamList
-        const nyamList = await reqNyamList.read();
-
-        // set markers
-        const org = nyamList.shift();
-        const temp = new window.naver.maps.Marker({
-            position: new window.naver.maps.LatLng(org.lat, org.lng),
-            map: map,
-            icon: {
-              url : `img/icons/${org.type}.png`,
-              size: new window.naver.maps.Size(48, 48),
-              origin: new window.naver.maps.Point(0, 0),
-            }
-        });
-
-        markers = nyamList.map( item => (
-            new window.naver.maps.Marker({
-                position: new window.naver.maps.LatLng(item.lat, item.lng),
-                map: map,
-                icon: {
-                  url : `img/icons/${item.type}.png`,
-                  size: new window.naver.maps.Size(32, 32),
-                  origin: new window.naver.maps.Point(0, 0),
-                }
-            })
-        ));
-
-        // set eventListener
-        map.addListener("click", (e) => {
-            const latlng = e.coord;
-            console.log("lat lng : ", latlng);
-        });
-    }
+    const [markers, setMarkers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     return (
         <MapsWrap className="Maps">
-            <ScriptTag type="text/javascript" onLoad={init} src={mapValues.mapSource} />
+            <ScriptTag type="text/javascript" onLoad={() => { init(setIsLoading, setMarkers); }} src={mapValues.mapSource} />
+
             <Map id="map"></Map>
+            <Loading isLoading={isLoading} />
         </MapsWrap>
     );
 }
@@ -98,4 +58,53 @@ const reqNyamList = {
     write : () => {
 
     },
+}
+
+const init = async(setIsLoading, setMarkers) => {
+    setIsLoading(true);
+
+    // set naver.maps.Map
+    const { center, zoom } = mapValues;
+
+    const mapOptions = {
+        center: new window.naver.maps.LatLng(center.lat, center.lng),
+        zoom: zoom,
+    };
+    map = new window.naver.maps.Map("map", mapOptions);   
+
+    // read nyamList
+    const nyamList = await reqNyamList.read();
+
+    // set markers
+    const org = nyamList.shift(); // organization
+    const org_marker = new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(org.lat, org.lng),
+        map: map,
+        icon: {
+          url : `img/icons/${org.type}.png`,
+          size: new window.naver.maps.Size(48, 48),
+          origin: new window.naver.maps.Point(0, 0),
+        }
+    });
+
+    const temp = nyamList.map( item => ( // nyam items
+        new window.naver.maps.Marker({
+            position: new window.naver.maps.LatLng(item.lat, item.lng),
+            map: map,
+            icon: {
+              url : `img/icons/${item.type}.png`,
+              size: new window.naver.maps.Size(32, 32),
+              origin: new window.naver.maps.Point(0, 0),
+            }
+        })
+    ));
+    setMarkers(temp);
+
+    // set eventListener
+    map.addListener("click", (e) => {
+        const latlng = e.coord;
+        console.log("lat lng : ", latlng);
+    });
+
+    setIsLoading(false);
 }
