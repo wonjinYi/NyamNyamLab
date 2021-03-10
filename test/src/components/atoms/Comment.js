6a9c48e3d1653b8ec3dde2c7e98453b8ba343c9f
wonjinYi <studioplug17@gmail.com>
2021-03-11 01:25:33 +0900

    comment layout

---
+++ test/src/components/atoms/Comment.js
@@ -0,0 +1,39 @@
+// imported Modules =============================================
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { Button, Tooltip } from "antd";
+import { DeleteOutlined } from "@ant-design/icons"
+
+// Main Component ===============================================
+export default function Comment ({ content, onDelete }) {
+<DeleteOutlined />
+    return (
+        <CommentWrap className="Comment">
+            <Content>{content}</Content>
+            <Tooltip className="deleteComment" title="크큭..없.앤.다." placement="right">
+                <Button shape="circle" icon={<DeleteOutlined />} style={{}} onClick={onDelete} />
+            </Tooltip>
+        </CommentWrap>
+    );
+}
+
+// style ========================================================
+const SPACE = "4px";
+
+const CommentWrap = styled.div`
+    display : flex;
+    justify-content : flex-end;
+    
+    margin-bottom : ${SPACE};
+    `;
+
+const Content = styled.p`
+    padding : ${SPACE};
+    border : 1px solid #DCDCDC;
+    border-radius : 8px;
+    margin : 0;
+    margin-right : ${SPACE};
+    `;
+
+// function =====================================================
--- test/src/components/followers/Maps/MapsModalComment.js
+++ test/src/components/followers/Maps/MapsModalComment.js
@@ -2,15 +2,34 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { Button, Tooltip, Input } from "antd";
+import { EditOutlined } from "@ant-design/icons"
+
 // imported components ==========================================
-// *
+import Comment from "../../atoms/Comment";
 
 // Main Component ===============================================
 export default function MapsModalComment ({ nyamListSoruce, selectedNyam }) {
+    const commentsData = JSON.parse(selectedNyam.comment).comment;
 
     return (
         <MapsModalCommentWrap className="MapsModalComment">
-            <h1>Comment</h1>
+            
+            <Comments>
+            {
+                commentsData.map( item => (
+                    <Comment content={item} onDelete={onDelete} />
+                ))
+            }
+            </Comments>
+
+            <Form>
+                <Input placeholder="새로운 의견을 적어주세요" style={{borderRadius:"8px", marginRight:"4px"}}></Input>
+                <Tooltip className="deleteComment" title="새로운 의견적기" placement="right">
+                    <Button shape="circle" icon={<EditOutlined />} size="normal" style={{}} onClick={onCreate} />
+                </Tooltip>
+            </Form>
+            
         </MapsModalCommentWrap>
     );
 }
@@ -18,6 +37,34 @@ export default function MapsModalComment ({ nyamListSoruce, selectedNyam }) {
 // style ========================================================
 const MapsModalCommentWrap = styled.div`
     display : flex;
+    flex-direction : column;
+
+    width : 40%;
+
+    margin-left : auto;
+    padding : 16px;
+    border : 1px solid #dbdbdb;
+    border-radius : 8px;
     `;
 
+const Comments = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-content : flex-end;
+    justify-content : flex-start;
+   
+    overflow-y: auto;
+`;
+
+const Form = styled.div`
+    display :flex;
+    align-items : center;
+`;
+
 // function =====================================================
+function onCreate() {
+    console.log("새로쓰기")
+}
+function onDelete() {
+    console.log("삭제삭제");
+}
